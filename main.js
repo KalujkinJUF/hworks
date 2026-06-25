b80406f28c7554f016c4fb4ab47c0984deb9a835
Kalujkin_JUF <dmitriyjari@gmail.com>
2026-06-26 02:31:02 +0300

    clean: remove dead Electron code from main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
@@ -10,42 +9,3 @@ process.on('uncaughtException', (error) => {
 
 // Запускаем Express сервер бэкенда
 require('./app.js');
-
-function createWindow() {
-    // Создаем окно браузера Chromium
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 800,
-        minWidth: 800,
-        minHeight: 600,
-        title: "Социальная сеть",
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true
-        }
-    });
-
-    // Загружаем локальный адрес Express сервера
-    win.loadURL('http://localhost:3000');
-
-    // Отключаем стандартную верхнюю панель меню (Файл, Правка...) для более современного вида
-    win.setMenuBarVisibility(false);
-}
-
-// Запуск приложения при готовности
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-});
-
-// Завершение работы при закрытии всех окон (кроме macOS)
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});

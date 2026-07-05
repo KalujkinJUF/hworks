6c564646ab23820de848943a36e10c82d006e65b
Kalujkin_JUF <dmitriyjari@gmail.com>
2026-07-06 00:11:22 +0300

    chore: bump API version to a0.2.6 in app.js

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get('/api/health', (req, res) => {
 });
 // Эндпоинт получения текущей версии
 app.get('/api/version', (req, res) => {
-    res.json({ version: 'a0.2.4' });
+    res.json({ version: 'a0.2.6' });
 });
 
 // Раздача всего из папки public
